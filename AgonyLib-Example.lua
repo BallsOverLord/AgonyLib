e1175117ff9525c280475b987cf8cabd5ea95d01
BallsOverLord <131700300+BallsOverLord@users.noreply.github.com>
2023-06-28 16:38:25 +0300

    Update AgonyLib-Example.lua

--- AgonyLib-Example.lua
+++ AgonyLib-Example.lua
@@ -1,4 +1,4 @@
-	local library = loadstring(game:HttpGet(""))()
+	local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/BallsOverLord/AgonyLib/main/AgonyLib.txt"))()
 	
 	UI.CreateBase() --Create UI
 
