cf25a3f09fea88d0071b6c42f0c31e27dabb6aa0
BallsOverLord <131700300+BallsOverLord@users.noreply.github.com>
2023-10-14 23:18:20 +0300

    Update AgonyLib-Example.lua

--- AgonyLib-Example.lua
+++ AgonyLib-Example.lua
@@ -4,12 +4,6 @@
 
 	--(Text)
 	--[[
-		Text = Message Displayed When F4 Is Pressed I Think
-	--]]
-	UpdateLeaveMessage("Text")
-
-	--(Text)
-	--[[
 		Text = Set Leave Message
 	--]]
 	UpdateLeaveMessage("Text")
